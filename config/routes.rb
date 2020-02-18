f03c5024bcaf5f2fd91fb891981ddfe9a435eda9
Ray Amable <rayamable@gmail.com>
2020-02-18 11:19:52 +0000

    add validation to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
    
-  root to: "home#index"
+  root 'posts#index'
 
 end
